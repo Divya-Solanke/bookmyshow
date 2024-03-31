bbab6ac9c0e2645c3a8713c7b4e5aed90079d556
Divya-Solanke <160421946+Divya-Solanke@users.noreply.github.com>
2024-03-31 17:20:28 +0530

    project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {Route, Routes} from 'react-router-dom'
+import { Header } from './Pages/Header';
+import { Movies } from "./Pages/Movies";
+// import { Stream } from "./Pages/Stream";
+import { Events } from "./Pages/Events";
+import { Plays } from "./Pages/Plays";
+import { Sports } from "./Pages/Sports";
+// import { Activites } from "./Pages/Activites";
+// import { Footer } from "./Footer/Footer";
+
+function App() {
+    return <div className="bg-light">
+        <Header />
+        <Routes>
+            <Route path="/" element={<Movies />}></Route>
+            {/* <Route path="/stream" element={<Stream />}></Route> */}
+            <Route path="/events" element={<Events />}></Route>
+            <Route path="/plays" element={<Plays />}></Route>
+            <Route path="/sports" element={<Sports />}></Route>
+            {/* <Route path="/activities" element={<Activites />}></Route> */}
+            <Route path="*" element={<h4>Page Not Found</h4>}></Route>
+        </Routes>
+        {/* <Footer /> */}
+    </div>
+}
+
+export default App
+
+
+
+
+
